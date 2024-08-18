bd82fa55b9fd684eaa5cc9b7f63dffb101c32c6c
itamarlevi <itamar@apiiro.com>
2024-08-18 13:14:46 +0300

    test

--- empty_handler.py
+++ empty_handler.py
@@ -13,27 +13,29 @@ ray_serve_logger = logging.getLogger("ray.serve")
 
 
 @ray.remote
-class HeavyComputeActor:
-    def heavy_compute(self, data):
-        # Simulate a heavy computation (e.g., 3 seconds of work)
-        time.sleep(3)
-        return {"result": f"Processed {data}"}
-
+class Actor:
+    async def heavy_compute(self):
+        # taking a long time...
+        # await asyncio.sleep(5)
+        return
 
 @serve.deployment()
 class RiskyReasoning:
     def __init__(self):
         ray_serve_logger.warning(f"1111111111111")
-        self.actor = HeavyComputeActor.remote()
+        self.actor = Actor.remote()
 
     def translate(self, text: str) -> str:
         return "bbbbbbbbbbbb"
 
     async def __call__(self, request: starlette.requests.Request):
         req = await request.json()
-        result_ref = self.actor.heavy_compute.remote(req["title"])
-        result = await ray.get(result_ref)
-
+        NUM_TASKS = 1000
+        result_refs = []
+        # When NUM_TASKS is large enough, this will eventually OOM.
+        for _ in range(NUM_TASKS):
+            result_refs.append(self.actor.heavy_compute.remote())
+        ray.get(result_refs)
         ray_serve_logger.warning(f"Missing title or description field in the json request")
         time.sleep(10)
         return f"hellooooooo "
