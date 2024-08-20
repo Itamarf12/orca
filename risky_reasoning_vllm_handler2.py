138e5f48ca02097369a0c3ded1d440e3c3914c5d
itamarlevi <itamar@apiiro.com>
2024-08-20 22:00:06 +0300

    test

--- risky_reasoning_vllm_handler2.py
+++ risky_reasoning_vllm_handler2.py
@@ -23,52 +23,19 @@ MODEL_NM = "microsoft/Orca-2-13b"
 DEVICE = 'auto' # 'cpu'
 
 
-@ray.remote
-class Actor:
-    async def heavy_compute(self):
-        # taking a long time...
-        # await asyncio.sleep(5)
-        return
 
 @serve.deployment()
 class RiskyReasoning:
     def __init__(self):
         ray_serve_logger.warning(f"1111111111111")
-        self.actor = Actor.remote()
+
 
     def translate(self, text: str) -> str:
         return "bbbbbbbbbbbb"
 
     async def __call__(self, request: starlette.requests.Request):
         req = await request.json()
-        NUM_TASKS = 1000
-        result_refs = []
-        # When NUM_TASKS is large enough, this will eventually OOM.
-        for _ in range(NUM_TASKS):
-            result_refs.append(self.actor.heavy_compute.remote())
-        ray.get(result_refs)
-        ray_serve_logger.warning(f"Missing title or description field in the json request")
-        time.sleep(10)
-        return f"hellooooooo1 "
-
-# @serve.deployment
-# class RiskyReasoning:
-#     def __init__(self):
-#         # Create a remote actor instance
-#         self.actor = HeavyComputeActor.remote()
-#
-#     async def __call__(self, request: starlette.requests.Request):
-#         # Get input data from the request
-#         data = await request.json()
-#
-#         # Submit work to the remote actor
-#         result_ref = self.actor.heavy_compute.remote(data["title"])
-#
-#         # Get the result (will block until result is ready)
-#         result = await ray.get(result_ref)
-#
-#         return result
-
+        return f"hellooooooo12 "
 
 
 app = RiskyReasoning.bind()
